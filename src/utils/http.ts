098a370a3be20aba43e67619f9297347116b0bbf
Ansaf Ahmed <me@ansaf.net>
2021-12-15 17:25:35 +1100

    update: v2 fees calc

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -7,13 +7,19 @@ export const POST = async (url: string, body: Record<string, any>) => {
 };
 
 export const SUBGRAPH = (query: string) => {
-  return POST('https://api.thegraph.com/subgraphs/name/1saf/perpetual-referral-subgraph', {
-    query
-  });
+  return POST(
+    'https://api.thegraph.com/subgraphs/id/QmcnxiDutrFXquaa7kHpExzHJBqMMxD6FQEtZ584c8mDxz',
+    {
+      query
+    }
+  );
 };
 
 export const STAKED_SUBGRAPH = (query: string) => {
-  return POST('https://api.thegraph.com/subgraphs/name/1saf/perpetual-staking-subgraph', {
-    query
-  });
+  return POST(
+    'https://api.thegraph.com/subgraphs/name/1saf/perpetual-staking-subgraph',
+    {
+      query
+    }
+  );
 };
